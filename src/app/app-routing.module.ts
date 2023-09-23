f8e6fa8405b07cedd9884a83cd36e41805bb12e9
Soni <soni@recalstudios.net>
2023-09-23 13:54:22 +0200

    feat: create basic components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    title: 'Home'
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+    title: 'Page not found'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
