7435bf9c4dd3721f384fa9c6486158b932a4c2b9
Soni <soni801_@outlook.com>
2023-10-11 14:03:40 +0200

    fix: highlight correct header element on page load

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,28 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent
-{
-  /**
-   * Marks an element in the header as the selected element by changing its classlist
-   *
-   * @param event The MouseEvent of the click
-   */
-  select(event: MouseEvent)
-  {
-    // Remove 'selected' class from all links in header
-    // Using querySelectorAll is probably not the best solution here, but it works well enough
-    document.querySelectorAll('app-header a').forEach(e => e.classList.remove('selected'));
+export class HeaderComponent {
 
-    // Check that the click target is an HTML element
-    if (event.target instanceof HTMLElement)
-    {
-      // Add the 'selected' class to the clicked element
-      event.target.classList.add('selected');
-    }
-  }
 }
