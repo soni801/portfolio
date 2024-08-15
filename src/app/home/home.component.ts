dbf217e3a19dc6a30bce79281b6299a8accdc582
Soni <soni@recalstudios.net>
2024-08-15 15:21:50 +0200

    feat: add footer

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,15 @@
 import {AfterViewInit, Component} from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
 import {LanguageDisplayComponent} from "../language-display/language-display.component";
+import {FooterComponent} from "../footer/footer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     NgOptimizedImage,
-    LanguageDisplayComponent
+    LanguageDisplayComponent,
+    FooterComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
