a20d920336347be6957201f39ca0a383c8735520
Soni <soni@recalstudios.net>
2025-01-02 22:37:41 +0100

    style: use text tacks in footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {TextTackComponent} from "../text-tack/text-tack.component";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    TextTackComponent
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
