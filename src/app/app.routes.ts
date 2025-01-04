d89fcc0f257acdfae6836fffe5195e36fd875351
Soni <soni@recalstudios.net>
2025-01-04 01:10:26 +0100

    style: set favicon and page title

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,9 @@ import { Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 
 export const routes: Routes = [
-  { path: '**', component: HomeComponent }
+  {
+    path: '**',
+    component: HomeComponent,
+    title: 'Samuel Aasen \u2022 Digital portefølje'
+  }
 ];
