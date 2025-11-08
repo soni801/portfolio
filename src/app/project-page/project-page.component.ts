8f6fd33f76c558c67c7f2068eb15426aee3ac258
Soni <soni@recalstudios.net>
2025-11-08 16:24:45 +0100

    feat: route projects

--- src/app/project-page/project-page.component.ts
+++ src/app/project-page/project-page.component.ts
@@ -1,10 +1,14 @@
 import {Component, inject} from '@angular/core';
 import {ActivatedRoute, RouterLink} from "@angular/router";
+import {KaffeDiemAppComponent} from "../project/kaffe-diem-app/kaffe-diem-app.component";
+import {RecalSocialComponent} from "../project/recal-social/recal-social.component";
 
 @Component({
   selector: 'app-project-page',
   imports: [
-    RouterLink
+    RouterLink,
+    KaffeDiemAppComponent,
+    RecalSocialComponent
   ],
   templateUrl: './project-page.component.html',
   styleUrl: './project-page.component.scss'
